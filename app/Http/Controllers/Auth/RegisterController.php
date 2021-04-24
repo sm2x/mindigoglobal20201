ef85c0fe7571646beabf0a07f7e7acfd575e08ac
vicsystems55 <victorasuquob@gmail.com>
2021-04-24 00:11:03 -0700

    direct referral

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Providers\RouteServiceProvider;
 use App\User;
 use App\DirectReferral;
 use App\Notification;
+use Carbon\Carbon;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
