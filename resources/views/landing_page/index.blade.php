b3a57f367110aeb952deba435078e1e6cd3c8405
vicsystems55 <victorasuquob@gmail.com>
2021-04-30 10:57:51 -0700

    landing page ui

--- resources/views/landing_page/index.blade.php
+++ resources/views/landing_page/index.blade.php
@@ -40,7 +40,7 @@
 		
 			<div class="banner-parallax" data-banner-height="650">
 				<img src="images/files/parallax-bg/img-6.jpg" alt="">
-				<div class="overlay-colored color-bg-dark opacity-85"></div><!-- .overlay-colored end -->
+				<div class="overlay-colored color-bg-dark opacity-40"></div><!-- .overlay-colored end -->
 				<div class="slide-content">
 					<div class="container">
 						<div class="row">
@@ -61,22 +61,38 @@
 										<br>
 										{{$user_data->name}}
 									</h1>
-									<p class="description">
-										I will love to introduce you to an amazing business opportunity.
-									</p>
+									<h5 class="font-weight-bold">
+									We're Helping Thousands of Hard Working People To Switch It Up And Become Online Entrepreneurs
+									</h5>
+									<h4 class=" font-weight-bold">
+									In this Web class, You Are About To Discover How To Start Your Own Successful ONLINE BUSINESS…WITH NO PREVIOUS EXPERIENCE!!!
+									</h4>
 									<div class="cta-subscribe cta-subscribe-1 box-form mt-40">
 										<div class="box-content">
 											<div class="cs-notifications">
 												<div class="cs-notifications-content"></div>
 											</div><!-- .cs-notifications end -->
-											<form id="form-email-subscribe" class="form-inline redirected">
+											<form id="" class="redirected">
+												<div class="form-group">
+													<i class="fas fa-envelope field-icon"></i>
+													<input type="email" name="esEmail" placeholder="Full name" id="esEmail"
+														class="form-control">
+												</div><!-- .form-group end -->
+
 												<div class="form-group">
 													<i class="fas fa-envelope field-icon"></i>
 													<input type="email" name="esEmail" placeholder="Email Address" id="esEmail"
 														class="form-control">
 												</div><!-- .form-group end -->
+
 												<div class="form-group">
-													<input style="background-color: #d49719;" type="submit" class="form-control btn-warning" value="Getstarted">
+													<i class="fas fa-envelope field-icon"></i>
+													<input type="email" name="esEmail" placeholder="Phone" id="esEmail"
+														class="form-control">
+												</div><!-- .form-group end -->
+												
+												<div class="form-group">
+													<input style="background-color: #d49719;" type="submit" class="form-control btn-warning" value="Get Started">
 												</div><!-- .form-group end -->
 											</form><!-- #form-email-subscribe end -->
 										</div><!-- .box-content end -->
@@ -105,7 +121,7 @@
 								<br>
 								We Deliver
 								<br>
-								<span class="heading-300">to Our Users</span>
+							
 							</h3>
 
 						</div><!-- .col-md-3 end -->
@@ -116,9 +132,9 @@
 									<i class="far fa-lightbulb"></i>
 								</div><!-- .box-icon end -->
 								<div class="box-content">
-									<h5><a href="javascript:;">Strategy & Planning</a></h5>
+									<h5><a href="javascript:;">Digital Marketing</a></h5>
 									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint ipsa voluptatibus.
+									Master the #1 Skillset For the Good life. Live, Work and Travel the world.
 									</p>
 								</div><!-- .box-content end -->
 							</div><!-- .box-info box-service-1 end -->
@@ -131,9 +147,9 @@
 									<i class="fas fa-chart-line"></i>
 								</div><!-- .box-icon end -->
 								<div class="box-content">
-									<h5><a href="javascript:;">Fast Development</a></h5>
+									<h5><a href="javascript:;">Online Business</a></h5>
 									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint ipsa voluptatibus.
+									Start an Online Business. Become your own boss and live life according to your terms
 									</p>
 								</div><!-- .box-content end -->
 							</div><!-- .box-info box-service-1 end -->
@@ -146,9 +162,9 @@
 									<i class="fas fa-check"></i>
 								</div><!-- .box-icon end -->
 								<div class="box-content">
-									<h5><a href="javascript:;">Product Launch</a></h5>
+									<h5><a href="javascript:;">Entrepreneurship</a></h5>
 									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint ipsa voluptatibus.
+									Become an Internet Entrepreneur.  Design the Life of your Dreams.
 									</p>
 								</div><!-- .box-content end -->
 							</div><!-- .box-info box-service-1 end -->
@@ -163,87 +179,12 @@
 		</div><!-- .section-flat end -->
 
 		<!-- === Section Our Services 2 =========== -->
-		<div id="section-our-services-2" class="section-flat">
-		
-			<div class="section-content">
-		
-				<div class="container">
-					<div class="row">
-						<div class="col-md-6">
-		
-							<div class="section-title mt-20 mt-md-0">
-									<h2>
-										<span class="heading-300">Boost The Growth of</span>
-										<br>
-										Your Startup Business
-									</h2>
-									<p class="description">
-										This should be used to tell a story and let your users know a little more about your product or service. How can you benefit them?
-									</p>
-							</div><!-- .section-title end -->
-							<ul class="list-info arrow-right">
-								<li>Donec nec justo eget felis facilisis fermentum.</li>
-								<li>Aliquam porttitor mauris sit amet orci.</li>
-								<li>Integer vitae libero ac risus egestas placerat.</li>
-							</ul><!-- .list-info.arrow-right end -->
-						
 
-						</div><!-- .col-md-6 end -->
-						<div class="col-md-6 mt-md-60">
-
-							<div class="img-featured-1 img-bg">
-								<img src="images/files/img-featured-1/img-1.jpg" alt="">
-							</div><!-- .img-featured-1 end -->
-
-						</div><!-- .col-md-6 -->
-						<div class="col-md-12">
-
-							<div class="divider-container">
-								<div class="divider-line">
-									
-								</div><!-- .divider-line end -->
-							</div><!-- .divider-container end -->
-
-						</div><!-- .col-md-12 end -->
-						<div class="col-md-6 col-md-push-6 pl-55">
-						
-							<div class="section-title mt-20 mt-md-0">
-								<h2>
-									<span class="heading-300">Introduce The Goals of Your</span>
-									<br>
-									Business or Company
-								</h2>
-								<p class="description">
-									This should be used to tell a story and let your users know a little more about your product or service. How
-									can you benefit them?
-								</p>
-							</div><!-- .section-title end -->
-							<ul class="list-info arrow-right">
-								<li>Donec nec justo eget felis facilisis fermentum.</li>
-								<li>Aliquam porttitor mauris sit amet orci.</li>
-								<li>Aenean dignissim pellentesque felis.</li>
-							</ul><!-- .list-info.arrow-right end -->
-							<a class="btn colorful large hover-dark rounded mt-20 move-top" href="#">Get Started</a>
-						
-						</div><!-- .col-md-6 end -->
-						<div class="col-md-6 col-md-pull-6 mt-md-60">
-						
-							<div class="img-featured-1 img-bg">
-								<img src="images/files/img-featured-1/img-2.jpg" alt="">
-							</div><!-- .img-featured-1 end -->
-						
-						</div><!-- .col-md-6 -->
-					</div><!-- .row end -->
-				</div><!-- .container end -->
-		
-			</div><!-- .section-content end -->
-		
-		</div><!-- .section-flat end -->
 
 		<!-- === Section Some Facts 1 =========== -->
-		<div id="section-some-facts-1" class="section-flat">
+		<div id="section-some-facts-1" class="section-flat ">
 		
-			<div class="section-content">
+			<div style="background-color: black" class="section-content ">
 		
 				<div class="container">
 					<div class="row">
@@ -266,42 +207,7 @@
 						<div class="col-md-6 mt-md-60">
 		
 							<ul class="list-some-facts list-boxes boxes-2">
-								<li>
-									<div class="box-info box-some-fact-1 text-white">
-										<div class="box-icon">
-											<i class="fas fa-coins"></i>
-										</div><!-- .box-icon end -->
-										<div class="box-content">
-											<h2><span class="counter-stats">1500</span></h2>
-											<h5>Projects Completed</h5>
-										</div><!-- .box-content end -->
-									</div><!-- .box-info box-some-fact-1 end -->
-		
-								</li>
-								<li>
-									<div class="box-info box-some-fact-1 text-white">
-										<div class="box-icon">
-											<i class="fas fa-smile-beam"></i>
-										</div><!-- .box-icon end -->
-										<div class="box-content">
-											<h2><span class="counter-stats">1420</span></h2>
-											<h5>Happy Clients</h5>
-										</div><!-- .box-content end -->
-									</div><!-- .box-info box-some-fact-1 end -->
-								
-								</li>
-								<li>
-									<div class="box-info box-some-fact-1 text-white">
-										<div class="box-icon">
-											<i class="fas fa-coffee"></i>
-										</div><!-- .box-icon end -->
-										<div class="box-content">
-											<h2><span class="counter-stats">1250</span></h2>
-											<h5>Cups of Coffee</h5>
-										</div><!-- .box-content end -->
-									</div><!-- .box-info box-some-fact-1 end -->
-								
-								</li>
+						
 								<li>
 									<div class="box-info box-some-fact-1 text-white">
 										<div class="box-icon">
@@ -309,7 +215,20 @@
 										</div><!-- .box-icon end -->
 										<div class="box-content">
 											<h2><span class="counter-stats">21600</span></h2>
-											<h5>Hours of Work</h5>
+											<h5>Online Businesses Created</h5>
+										</div><!-- .box-content end -->
+									</div><!-- .box-info box-some-fact-1 end -->
+								
+								</li>
+
+								<li>
+									<div class="box-info box-some-fact-1 text-white">
+										<div class="box-icon">
+											<i class="fas fa-business-time"></i>
+										</div><!-- .box-icon end -->
+										<div class="box-content">
+											<h2><span class="counter-stats">21600</span></h2>
+											<h5>Members</h5>
 										</div><!-- .box-content end -->
 									</div><!-- .box-info box-some-fact-1 end -->
 								
@@ -339,10 +258,7 @@
 									<br>
 									Customers Testimonials
 								</h2>
-								<p class="description">
-									This should be used to tell a story and let your users know a little more about your product or service. How
-									can you benefit them?
-								</p>
+							
 							</div><!-- .section-title end -->
 
 						</div><!-- .col-md-6 end -->
@@ -361,15 +277,15 @@
 													<i class="fa fa-star"></i>
 												</div><!-- .rating end -->
 												<div class="ts-content">
-													Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia.
+												I've been able to start and run my own online business. Mentoring other people from different parts of the world, courtesy of PureWeb Academy Community.
 												</div><!-- .ts-content end -->
 												<div class="ts-person">
 													<div class="ts-img">
-														<img src="images/files/sliders/clients-testimonials/img-1.jpg" alt="">
+														<img src="images/files/sliders/clients-testimonials/prince.png" alt="">
 													</div><!-- .ts-img end -->
 													<div class="ts-name">
-														<h5>Larry Todd</h5>
-														<span>Art Director, Envato Market</span>
+														<h5>Prince Ikilowei</h5>
+														<span>Nigeria</span>
 													</div><!-- .ts-name end -->
 												</div><!-- .ts-person end -->
 											</div><!-- .testimonial-single-1 -->
@@ -386,14 +302,14 @@
 													<i class="fa fa-star"></i>
 												</div><!-- .rating end -->
 												<div class="ts-content">
-													Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia.
+												Coming in contact with PureWeb Academy team was the best thing that has happened to me, My perspective on life and business has changed
 												</div><!-- .ts-content end -->
 												<div class="ts-person">
 													<div class="ts-img">
-														<img src="images/files/sliders/clients-testimonials/img-2.jpg" alt="">
+														<img src="images/files/sliders/clients-testimonials/jenny.png" alt="">
 													</div><!-- .ts-img end -->
 													<div class="ts-name">
-														<h5>Maud Boyd</h5>
+														<h5>Jenny Cole</h5>
 														<span>Team Leader, Facebook</span>
 													</div><!-- .ts-name end -->
 												</div><!-- .ts-person end -->
@@ -411,15 +327,15 @@
 													<i class="fa fa-star"></i>
 												</div><!-- .rating end -->
 												<div class="ts-content">
-													Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia.
+												These guys are amazing. I haven't seen any training that has exceeded my expectation than this. I highly recommend them
 												</div><!-- .ts-content end -->
 												<div class="ts-person">
 													<div class="ts-img">
-														<img src="images/files/sliders/clients-testimonials/img-3.jpg" alt="">
+														<img src="images/files/sliders/clients-testimonials/brian.png" alt="">
 													</div><!-- .ts-img end -->
 													<div class="ts-name">
-														<h5>Paul Scrivens</h5>
-														<span>Web Developer, Google</span>
+														<h5>Brian </h5>
+														<span>United States</span>
 													</div><!-- .ts-name end -->
 												</div><!-- .ts-person end -->
 											</div><!-- .testimonial-single-1 -->
@@ -436,15 +352,15 @@
 													<i class="fa fa-star"></i>
 												</div><!-- .rating end -->
 												<div class="ts-content">
-													Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia.
+												PureWeb Academy team taught me digital marketing, entrepreneurship and online business. I now operate a technology and training company, training and empowering youths and women all over the country
 												</div><!-- .ts-content end -->
 												<div class="ts-person">
 													<div class="ts-img">
-														<img src="images/files/sliders/clients-testimonials/img-1.jpg" alt="">
+														<img src="images/files/sliders/clients-testimonials/aisha.png" alt="">
 													</div><!-- .ts-img end -->
 													<div class="ts-name">
-														<h5>Larry Todd</h5>
-														<span>Art Director, Envato Market</span>
+														<h5>Aisha Kabir Gaya</h5>
+														<span>Nigeria</span>
 													</div><!-- .ts-name end -->
 												</div><!-- .ts-person end -->
 											</div><!-- .testimonial-single-1 -->
@@ -487,67 +403,13 @@
 		</div><!-- .section-flat end -->
 
 		<!-- === Section Clients 1 =========== -->
-		<div id="section-clients-1" class="section-flat">
-		
-			<div class="section-content">
-		
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-		
-							<div class="slider-clients">
-								<ul class="slick-slider">
-									<li>
-										<div class="client-single">
-											<a href="javascript:;"><img src="images/files/sliders/clients/img-1.png" alt=""></a>
-										</div>
-									</li>
-									<li>
-										<div class="client-single">
-											<a href="javascript:;"><img src="images/files/sliders/clients/img-2.png" alt=""></a>
-										</div>
-									</li>
-									<li>
-										<div class="client-single">
-											<a href="javascript:;"><img src="images/files/sliders/clients/img-3.png" alt=""></a>
-										</div>
-									</li>
-									<li>
-										<div class="client-single">
-											<a href="javascript:;"><img src="images/files/sliders/clients/img-4.png" alt=""></a>
-										</div>
-									</li>
-									<li>
-										<div class="client-single">
-											<a href="javascript:;"><img src="images/files/sliders/clients/img-5.png" alt=""></a>
-										</div>
-									</li>
-									<li>
-										<div class="client-single">
-											<a href="javascript:;"><img src="images/files/sliders/clients/img-6.png" alt=""></a>
-										</div>
-									</li>
-									<li>
-										<div class="client-single">
-											<a href="javascript:;"><img src="images/files/sliders/clients/img-2.png" alt=""></a>
-										</div>
-									</li>
-								</ul><!-- .slick-slider end -->
-							</div><!-- .slider-clients end -->
-		
-						</div><!-- .col-md-12 end -->
-					</div><!-- .row end -->
-				</div><!-- .container end -->
-		
-			</div><!-- .section-content end -->
-		
-		</div><!-- .section-flat end -->
+
 
 		<!-- === Section CTA 1 =========== -->
 		<div id="section-cta-1" class="section-parallax text-white text-center">
 		
 			<img src="images/files/parallax-bg/img-2.jpg" alt="">
-			<div class="overlay-colored color-bg-dark opacity-85"></div><!-- .overlay-colored end -->
+			<div class="overlay-colored color-bg-dark opacity-50"></div><!-- .overlay-colored end -->
 			<div class="section-content">
 		
 				<div class="container">
@@ -555,12 +417,11 @@
 						<div class="col-md-6 col-md-offset-3">
 		
 							<h1>
-								<span class="heading-300">We Are Waiting to</span>
-								<br>
-								Listen From You!
+								
+								Exprience freedom and timeless wisdom...
 							</h1>
 							<p class="description">
-								We care to deliver adaptive services and solutions to our customers to achieve and satisfy their needs.
+							
 							</p>
 							<a class="btn x-large colorful hover-dark rounded mt-20 move-top" href="#">Get Started</a>
 		
